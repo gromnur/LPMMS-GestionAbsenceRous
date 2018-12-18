81d895fcc67997e96e7c906f9a81263765654684
Flavien <flavienregis@gmail.com>
2018-12-18 22:49:20 +0100

    Ajout entête function

---
+++ www/DAO/DAOCours.php
@@ -0,0 +1,32 @@
+<?php
+
+require('AccesBDD.php');
+require('DAOGroupeEtudiant.php');
+require('DAOMatiere.php');
+
+/*
+ * Créés des cours avec leur nom, prenom, idGroupe et ine
+ * Return true si créé/existant, false si erreur
+ */
+function createCours($id_matiere, $id_groupe, $horaireDebut, $horaireFin) {
+    // TODO createCours
+    // créé cours
+}
+
+/*
+ * Return la liste des cours [$nom, $prenom, $ine]
+ */
+function selectCours($dateDebut,$dateFin, $id_groupe = null, $id_filiere = null, $id_departement = null, $id_matiere = null, $id_professeur = null) {
+    // TODO coder selectAvecFiliereCours
+    // Construire la requete en fonction des different argument passé pour affiner le resultat
+}
+
+/*
+ * Delete tout les cours lier à un departement selectionné
+ */
+function dropDepartementCours($id_departement) {
+    // TODO dropDepartementCours
+    // Faire une grosse jointure et supprimer
+
+}
+?>
--- www/DAO/DAOSalle.php
+++ www/DAO/DAOSalle.php
@@ -6,9 +6,9 @@ require('AccesBDD.php');
  * Créé un salle
  * Renvoi true si inserer, false sinon
  */
-function createSalle($numero) {
+function createSalle($numero_salle) {
 
-    // Verifier si le $numero est present
+    // Verifier si le $numero_salle est present
         // Si present renvoye false
 
     // Creation d'un salle
@@ -16,8 +16,8 @@ function createSalle($numero) {
 }
 
 /*
- * Return $numero si present, 0 Sinon
+ * Return $numero_salle si present, 0 Sinon
  */
-function numeroExisteSalle($numero) {
-    // TODO coder libelleExisteSalle
+function numeroExisteSalle($numero_salle) {
+    // TODO coder numeroExisteSalle
 }
