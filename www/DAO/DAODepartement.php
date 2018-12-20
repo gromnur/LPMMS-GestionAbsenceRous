7dd8eddee0d788886631357b906eeafd5f035036
Flavien <flavienregis@gmail.com>
2018-12-20 14:44:29 +0100

    Tout les libelle doivent être unique

--- www/DAO/DAODepartement.php
+++ www/DAO/DAODepartement.php
@@ -12,7 +12,23 @@ function createDepartement($libelle) {
         // Si present renvoye false
 
     // Creation d'un departement
+    // récupération accés base de données
+    $bd = getConnexion();
+    $rqt = "INSERT INTO departement(libelle) VALUES (:libelle)";
+    $stmt = $bd->prepare($rqt);
+    // ajout param
+    $stmt->bindParam(:libelle, $libelle);
+    // execution requette
+    $stmt->execute();
 
+    // récupération resultat
+    $listResult = $stmt->fetchAll();
+
+    if (count($listResult) == 0) {
+        return 0;
+    } else {
+        return $listResult[0];
+    }
 }
 
 /*
@@ -25,8 +41,24 @@ function selectDepartement () {
 /*
  * Return id_departement si present, 0 Sinon
  */
-function libelleExisteDepartement($libelle) {
-    // TODO coder libelleExisteDepartement
+function libelleExisteDepartement($libelle) {.
+    // récupération accés base de données
+    $bd = getConnexion();
+    $rqt = "SELECT id_departement, libelle FROM departement WHERE libelle = :libelle";
+    $stmt = $bd->prepare($rqt);
+    // ajout param
+    $stmt->bindParam(:libelle, $libelle);
+    // execution requette
+    $stmt->execute();
+
+    // récupération resultat
+    $listResult = $stmt->fetchAll();
+
+    if (count($listResult) == 0) {
+        return 0;
+    } else {
+        return $listResult[0];
+    }
 }
 
 /*
